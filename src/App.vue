fef6b9cfe219b33c71764dbe99ba10b4f276994b
xuegao <hubeixuegao@gamil.com>
2022-06-27 14:56:53 +0800

    vuex

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,52 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <button @click="incr">incr</button>
+    <button @click="ChatIncrement">ChatIncrement</button>
+    <button @click="ChatV2Increment">ChatV2Increment</button>
+    <div>defaultCount = {{ defaultCount }}</div>
+    <div>chatCount = {{ chatCount }}</div>
+    <div>chatCountV2 = {{ chatCountV2 }}</div>
+    <button @click="getChatCountV2">getChatCountV2 = {{ getChatCountV2Result }}</button>
+    <div>getChatCountV2Result = {{ getChatCountV2Result }}</div>
+    <input type="text" v-model="getChatCountV2Result">
+  </div>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+<script lang="ts" setup>
+import {computed} from "vue";
+import {useStore} from "vuex";
+const store = useStore()
 
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
+console.log("store", store);
+let defaultCount = computed(() => store.state.defaultCount)
+console.log("defaultCount", defaultCount);
+let chatCount = computed(() => store.state.chatCount)
+console.log("chatCount", chatCount);
+let chatCountV2 = computed(() => store.state.CHAT_V2.chatCountV2)
+console.log("chatCountV2", chatCountV2);
+
+function getChatCountV2() {
+  let getChatCountV2Result = store.getters.getChatCountV2
+  console.log("[xuegao-vuex4-ts][App.vue][getChatCountV2Result=s%]", getChatCountV2Result)
+  return getChatCountV2Result
+}
+let getChatCountV2Result = getChatCountV2()
+
+function incr() {
+  // 使用 mutation
+  store.commit('increment')
+}
+
+function ChatIncrement() {
+  // 使用 mutation
+  store.commit('ChatIncrement')
+}
+
+function ChatV2Increment() {
+  // 使用 mutation
+  store.commit('ChatV2Increment')
+}
 </script>
 
 <style>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,53 @@
-import { createStore } from 'vuex'
+import {createStore} from 'vuex'
+
+const CHAT = {
+    state: {
+        chatCount: 0,
+    },
+    mutations: {
+        increment() {
+            console.log("CHAT increment")
+            CHAT.state.chatCount++;
+        },
+        ChatIncrement() {
+            console.log("CHAT ChatIncrement")
+            CHAT.state.chatCount++;
+        },
+    },
+};
+const CHAT_V2 = {
+    state: {
+        chatCountV2: 0,
+    },
+    mutations: {
+        increment() {
+            console.log("CHAT_V2 increment")
+            CHAT_V2.state.chatCountV2++;
+        },
+        ChatV2Increment() {
+            console.log("CHAT ChatV2Increment")
+            const number = CHAT_V2.state.chatCountV2 + 1
+            console.log("[xuegao-vuex4-ts][index.ts][number=s%]", number)
+            CHAT_V2.state.chatCountV2 = number
+            console.log("[xuegao-vuex4-ts][index.ts][chatCountV2=s%]", CHAT_V2.state.chatCountV2)
+        },
+    },
+    getters: {
+        getChatCountV2() {
+            console.log("[xuegao-vuex4-ts][index.ts][getChatCountV2=%s]", CHAT_V2.state.chatCountV2)
+            return CHAT_V2.state.chatCountV2
+        }
+    }
+};
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        defaultCount: 0
+    },
+    getters: {},
+    mutations: {},
+    actions: {},
+    modules: {
+        CHAT, CHAT_V2
+    }
 })
